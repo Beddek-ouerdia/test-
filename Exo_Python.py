d6e9eefa69ff0001290e2563fb8400dd5040f0d6
Beddek-ouerdia <125572423+Beddek-ouerdia@users.noreply.github.com>
2023-02-16 17:02:03 +0100

    Add files via upload

---
+++ Exo_Python.py
@@ -0,0 +1,27 @@
+#une variable de type entier
+x=5;
+#une variable de type reel
+y=5.5;
+
+#une chaine de caractere
+b="bonjour";
+
+#une matrice
+matrice=[[1,2,5],[2,8,9],[9,0,0]];
+
+
+n=("ameli","ouerdia","djoudjou","sara");
+
+#une variable de type char
+z='\n';
+
+p=" ";
+
+m=('korso','beddek');
+
+# variables booleennes
+t=True;
+f=False;
+
+q=int(input("introduire votre age:" ))
+s=float(input("introduire un reel:" ))
---
+++ fct.py
@@ -0,0 +1,14 @@
+#creation de la fonction qui s'appelle(ma_fonction)
+def ma_fonction():
+    #la saisie des variables par l'utilisateur 
+    a=int(input("donnez a:"))
+    b=int(input("donnez b:"))
+    #les operation arithmitiques(addition et multiplication)
+    c=a+b
+    t=a*b
+    #l'affichage des resultats 
+    print(t)
+    print(c)
+ma_fonction() 
+
+
